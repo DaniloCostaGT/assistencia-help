3fb1ddb505289015babe99c9f5545c8447d7e006
DaniloCostaGT <poe2005@gmail.com>
2026-08-24 11:28:43 -0300

    erro do admin

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,24 @@ import App from './App.tsx';
 import './index.css';
 import { ThemeProvider } from '@/contexts/ThemeContext';
 
+// Força o cancelamento e remoção de qualquer Service Worker / Cache antigo gravado no navegador
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
+// Limpa caches do navegador criados pela API CacheStorage, se existirem
+if ('caches' in window) {
+  caches.keys().then((names) => {
+    for (const name of names) {
+      caches.delete(name);
+    }
+  });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
